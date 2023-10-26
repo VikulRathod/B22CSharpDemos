a62c58d0ff45834ca2ea9e44e659aa7116992605
vikul rathod <vikul.rathod.net@gmail.com>
2023-10-26 13:22:52 +0530

    c# session 3

---
+++ 4_StringType/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_StringType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Vishal";
+            Console.WriteLine(name);
+
+            Console.WriteLine(name.Length);
+
+            name = "       Vishal";
+            Console.WriteLine(name);
+            Console.WriteLine(name.Trim());
+
+            name = "Vishal";
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.ToLower());
+
+            name = "\"Vishal\"";
+            Console.WriteLine(name); // "Vishal"
+
+            name = "\'Vishal\'";
+            Console.WriteLine(name);
+
+            name = "\\Vishal\\";
+            Console.WriteLine(name); // \Vishal\
+
+            Console.WriteLine("Vishal Pawar");
+            Console.WriteLine("Vishal\tPawar");
+            Console.WriteLine("Vishal\nPawar");
+
+            // string path = "E:\\temp\\Batch22\\Client";
+            string path = @"E:\temp\Batch22\Client";
+            Console.WriteLine(path);
+
+            string firstName = "Vihaan";
+            string lastName = "Rathod";
+            string fullName = firstName + lastName;
+            Console.WriteLine(fullName); // VihaanRathod
+
+            fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName); // Vihaan Rathod
+
+            fullName = string.Concat(firstName, " ", lastName);
+            Console.WriteLine(fullName); // Vihaan Rathod
+
+            string middleName = "Vikul";
+            fullName = string.Concat(firstName + " " + middleName + " " + lastName);
+            Console.WriteLine(fullName); // Vihaan Vikul Rathod
+
+            fullName = string.Join(" ", firstName, middleName, lastName);
+            Console.WriteLine(fullName); // Vihaan Vikul Rathod
+
+            // Full Name : Vihaan Vikul Rathod
+
+            fullName = string.Join(" ", "Full", "Name", ":", firstName, middleName, lastName);
+            Console.WriteLine(fullName);
+
+            // placeholder syntax
+            fullName = string.Format("Full Name : {0} {1} {2}", firstName, middleName, lastName);
+            Console.WriteLine(fullName);
+
+            // string interpolation
+            fullName = $"Full Name : {firstName} {middleName}\t, {lastName}";
+            Console.WriteLine(fullName);
+
+            fullName = $"My Son Name Is : {firstName}";
+            Console.WriteLine(fullName);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 5_TypeConversion/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10, j = 10;
+            Console.WriteLine(i + j); // 20
+
+            // int => string
+            string a = i.ToString(); // int -> string
+            string b = j.ToString();
+            Console.WriteLine(a + b); // 1010
+
+            byte b1 = 10;
+            short s1 = b1; // byte -> short
+            Console.WriteLine(s1); // 10
+
+            float f1 = s1; // short -> float
+            Console.WriteLine(f1); // 10
+
+            // int i1 = f1; // float -> int
+            int i1 = (int)f1; // float -> int
+            Console.WriteLine(i1);
+
+            f1 = 25.55f;
+            i1 = (int)f1;
+            Console.WriteLine(i1); // 25.55
+
+            string s = "2000";
+            // string s = "ABCD";
+            // i1 = s; // string -> int
+            i1 = Convert.ToInt32(s);
+            Console.WriteLine(i1); // 100
+
+            s = "true";
+            bool bb = Convert.ToBoolean(s); // string -> bool
+            Console.WriteLine(bb);
+
+
+            s = "777";
+            // Parse()
+            // Parse() method throws error when string is not in correct format
+            i1 = int.Parse(s); // string -> int
+            Console.WriteLine(i1); // 777
+
+            //s = "XYZ";
+            //i1 = int.Parse(s); // runtime error
+            //Console.WriteLine(i1);
+
+            // TryParse() -> it handles the runtime error
+            // convert and return conversion status along with converted value
+            // in case of runtime error it returns false, default value as output
+            // default value of bool is false
+            // default value of int is 0
+            // default value of string is null
+
+            s = "2000";
+            bb = int.TryParse(s, out i1);
+            Console.WriteLine($"Status : {bb} Result : {i1}");
+
+            s = "XYZ";
+            bb = int.TryParse(s, out i1);
+            Console.WriteLine($"Status : {bb} Result : {i1}"); // false, 0
+
+            decimal x = 10.52m;
+            long l = (long) x; // decimal -> long
+            Console.WriteLine(l); // 10
+
+            Console.ReadLine();
+        }
+    }
+}
