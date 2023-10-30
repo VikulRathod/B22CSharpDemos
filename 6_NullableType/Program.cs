5cbbf61811fce26b3974fa312f9a12ab16371a1a
vikul rathod <vikul.rathod.net@gmail.com>
2023-10-30 12:22:42 +0530

    c# session 4

---
+++ 6_NullableType/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_NullableType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "vishal";
+            Console.WriteLine(name);
+
+            name = null; // nullable
+            Console.WriteLine(name);
+
+            // int = non nullable
+            // int? = nullable
+
+            int? age = null; // non-nullable
+            bool? isDelivered = null; // non-nullable
+            DateTime? dob = null; // non-nullable
+
+            Nullable<int> marks = null; // nullable
+
+            // int? = all int type values + null value
+
+            int? a = null;
+            // int b = (int) a; // int? => int
+            int b = a ?? 0;
+            Console.WriteLine($"a : {a} b : {b}");
+
+            // string email = "ajay@gmail.com";
+             string email = null;
+            string emailInUnpper = email?.ToUpper();
+            Console.WriteLine($"email: {email}\nemail in upper : {emailInUnpper}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 7_ConditionalStatements/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ConditionalStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 12;
+
+            //if (i == 10)
+            //{
+            //    Console.WriteLine($"{i} : TEN");
+            //    // Console.WriteLine("EVEN");                
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{i} : NOT TEN");
+            //}
+
+            // Console.WriteLine($"{i} : NOT TEN");
+
+            // ternary operator
+            // (condition) ? statement1(true) : statement2(false);
+
+            //i = 10;
+            //string result = (i == 10) ? "TEN" : "NOT TEN";
+            //Console.WriteLine($"{i} : {result}");
+
+            // if-else if-else ladder
+
+            //i = 10;
+            //if (i == 10)
+            //{
+            //    Console.WriteLine($"{i} : TEN");
+            //}
+            //else if (i == 20)
+            //{
+            //    Console.WriteLine($"{i} : TWENTY");
+            //}
+            //else if (i == 30)
+            //{
+            //    Console.WriteLine($"{i} : THIRTY");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{i} : NOT 10 20 or 30");
+            //}
+
+            // switch
+            //i = 100;
+            //switch (i)
+            //{
+            //    case 10:
+            //        Console.WriteLine($"{i} : TEN");
+            //        break;
+            //    case 20:
+            //        Console.WriteLine($"{i} : TWENTY");
+            //        break;
+            //    case 30:
+            //        Console.WriteLine($"{i} : THIRTY");
+            //        break;
+            //    default:
+            //        Console.WriteLine($"{i} : NOT 10 20 or 30");
+            //        break;
+            //}
+
+            int a = 10, b = 11;
+            if (a > b)
+            {
+                Console.WriteLine($"{a} is greater than {b}");
+            }
+            else if (b > a)
+            {
+                Console.WriteLine($"{b} is greater than {a}");
+            }
+            else
+            {
+                Console.WriteLine($"{a} and {b} are equal");
+            }
+
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Today);
+
+            Console.ReadLine();
+        }
+    }
+}
