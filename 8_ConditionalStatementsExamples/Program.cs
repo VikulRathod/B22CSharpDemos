3c38b6ccead83c44ed159d8419010270d751df51
vikul rathod <vikul.rathod.net@gmail.com>
2023-10-31 12:26:22 +0530

    c# session 5

---
+++ 8_ConditionalStatementsExamples/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_ConditionalStatementsExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please enter first number");
+            //int first = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Please enter second number");
+            //int second = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Please enter third number");
+            //int third = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"You entered : {first}, {second}, {third}");
+
+            #region nested if else
+
+            //if(first > second)
+            //{
+            //    if(first > third)
+            //    {
+            //        Console.WriteLine($"{first} is greater number");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{third} is greater number");
+            //    }
+            //}
+            //else if(second > first)
+            //{
+            //    if(second > third)
+            //    {
+            //        Console.WriteLine($"{second} is greater number");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{third} is greater number");
+            //    }
+            //}
+            //else
+            //{
+            //    if(first > third)
+            //    {
+            //        Console.WriteLine($"{first}, {second} are greater number");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{third} is greater number");
+            //    }
+            //}
+
+            #endregion nested if else
+
+            #region nested ternary operator
+            //// int number = (first > second) ? first : second;
+            //int number = (first > second) ? 
+            //    ((first > third) ? first : third) : 
+            //    ((second > third) ? second : third);
+
+            //Console.WriteLine($"{number} is greater number");
+
+            #endregion nested ternary operator
+
+            #region divisible 3 & 5
+            //// if input number is divisible by 3 = THREE
+            //// if input number is divisible by 5 = FIVE
+            //// if input number is divisible by 3 & 5 = THREEFIVE
+
+            //Console.WriteLine("Please enter a number");
+            //int num = int.Parse(Console.ReadLine());
+
+            //if (num % 3 == 0 && num % 5 == 0)
+            //{
+            //    Console.WriteLine("THREEFIVE");
+            //}
+            //else if (num % 3 == 0)
+            //{
+            //    Console.WriteLine("THREE");
+            //}
+            //else if (num % 5 == 0)
+            //{
+            //    Console.WriteLine("FIVE");
+            //}
+
+            #endregion divisible 3 & 5
+
+            #region even / odd number
+
+            //Console.WriteLine("Please enter a number");
+            //int num = int.Parse(Console.ReadLine());
+
+            //string result = (num % 2 == 0) ? "EVEN" : "ODD";
+
+            //Console.WriteLine($"{num} is {result} number");
+
+            #endregion even / odd number
+
+            #region grade meaning
+
+            Console.WriteLine("Please enter a grade");
+            char grade = char.Parse(Console.ReadLine().ToUpper());
+
+            switch (grade)
+            {
+                case 'A':
+                    Console.WriteLine($"{grade} : Distinction");
+                    break;
+                case 'B':
+                    Console.WriteLine($"{grade} : First Class");
+                    break;
+                case 'C':
+                    Console.WriteLine($"{grade} : Second Class");
+                    break;
+                case 'F':
+                    Console.WriteLine($"{grade} : Failed");
+                    break;
+                default:
+                    Console.WriteLine($"{grade} : Invalid Grade");
+                    break;
+            }
+
+            #endregion grade meaning
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 9_LoopingStatements/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_LoopingStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region while loop
+
+            //int start = 1; // initialization
+            //while (start <= 0)  // condition
+            //{
+            //    Console.WriteLine(start);
+            //    start++; // updation
+            //}
+
+            #endregion while loop
+
+            #region do while loop
+
+            //int start = 1;
+            //do
+            //{
+            //    Console.WriteLine(start);
+            //    start++;
+            //} while (start <= 0);
+
+            #endregion do while loop
+
+            #region for loop
+
+            //for (int start = 1; start <= 10; start++)
+            //{
+            //    Console.WriteLine(start);
+            //}
+
+            //for (int start = 1; start <= 10; start++)
+            //{
+            //    //if(start == 5)
+            //    //{
+            //    //    break;
+            //    //    // continue;
+            //    //}
+
+            //    Console.WriteLine(start);
+            //    if (start == 5)
+            //    {
+            //        start = 10;
+            //        // break;
+            //        // continue;
+            //    }
+            //}
+
+            #endregion for loop
+
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= 5; j++)
+            //    {
+            //        Console.Write($"*\t");
+            //    }
+            //    Console.WriteLine(); // new line
+            //}
+
+            //for (int i = 5; i > 0; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write($"*\t");
+            //    }
+            //    Console.WriteLine(); // new line
+            //}
+
+            for (int i = 5; i > 0; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{j}\t");
+                }
+                Console.WriteLine(); // new line
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
