163645a3f40c15072699f542ed138d3cada9bb5d
vikul rathod <vikul.rathod.net@gmail.com>
2023-12-15 12:41:20 +0530

    c# session 29

---
+++ 51_ExtensionMethod/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _51_ExtensionMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region string type
+
+            //int i = 10;
+
+            //string name = "vishal";
+            //string r1 = name.ToUpper();
+            //Console.WriteLine(r1);
+
+            //r1 = name.ToLower();
+            //Console.WriteLine(r1);
+
+            //r1 = name.ToCapitalCase(); // capital case
+            //Console.WriteLine(r1);
+
+            //string s = "vhaash";
+            //r1 = s.ToCapitalCase();
+            //Console.WriteLine(r1);
+
+            #endregion string type
+
+            BMW o1 = new BMW();
+            o1.Print("A"); // extension method
+
+            AUDI o2 = new AUDI();
+            o2.Print("B");
+            
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class StringHelper
+    {
+        public static string ToCapitalCase(this string name)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                if(i == 0)
+                {
+                    result += name[i].ToString().ToUpper();
+                }
+                else
+                {
+                    result += name[i];
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public interface ICar { }
+
+    public class BMW : ICar
+    {
+        public int Id { get; set; }
+        public static void PrintA()
+        {
+            // Console.WriteLine(Id);
+        }
+        public void PrintB()
+        {
+            Console.WriteLine(this.Id);
+        }
+
+        //public void Print()
+        //{
+        //    Console.WriteLine($"Print() : {GetType().Name}");
+        //}
+    }
+
+    public class AUDI : ICar
+    {
+        //public void Print()
+        //{
+        //    Console.WriteLine($"Print() : {GetType().Name}");
+        //}
+    }
+
+    public static class CarHelper
+    {
+        public static void Print(this ICar o, string s)
+        {
+            Console.WriteLine($"Print() : {s} : {o.GetType().Name}");
+        }
+    }
+}
---
+++ 52_CollectionClasses/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _52_CollectionClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region array
+
+            //int[] numbers = new int[2];
+            //numbers[0] = 10;
+            //numbers[1] = 20;
+            //// numbers[2] = 30;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            #endregion array
+
+            #region Arraylist
+
+            //// Console.BackgroundColor = ConsoleColor.Green;
+
+            //ArrayList numbers = new ArrayList(2);
+            //numbers.Add(10);
+            //numbers.Add(20);
+            //numbers.Add(30);
+
+            //Console.WriteLine("*** Original Collection ***");
+
+            ////for (int i = 0; i < numbers.Count; i++)
+            ////{
+            ////    Console.Write($"{numbers[i]} ");
+            ////}
+
+            //foreach (int item in numbers) // forward only
+            //{
+            //    Console.Write($"{item} ");
+            //}
+
+            //Console.WriteLine();
+
+            //// add multiple items
+            //ICollection temp = new ArrayList() { 40, 50, 60 };
+            //numbers.AddRange(temp);
+
+            //Console.WriteLine("*** AddRange(temp) Collection ***");
+
+            ////for (int i = 0; i < numbers.Count; i++)
+            ////{
+            ////    Console.Write($"{numbers[i]} ");
+            ////}
+
+            //foreach (var num in numbers)
+            //{
+            //    Console.Write($"{num} ");
+            //}
+
+            //Console.WriteLine();
+
+            //// 10 20 30 40 50 60
+
+            //// numbers.Add(15);
+            //numbers.Insert(1, 15);
+
+            //Console.WriteLine("*** Insert(1, 15) Collection ***");
+
+            ////for (int i = 0; i < numbers.Count; i++)
+            ////{
+            ////    Console.Write($"{numbers[i]} ");
+            ////}
+
+            //foreach (object item in numbers)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine();
+
+            //ICollection temp1 = new ArrayList() { 16, 17, 18 };
+            //numbers.InsertRange(2, temp1);
+
+            //Console.WriteLine("*** InsertRange(2, temp1) Collection ***");
+
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    Console.Write($"{numbers[i]} ");
+            //}
+            //Console.WriteLine();
+
+            //bool b1 = numbers.Contains(19);
+            //Console.WriteLine($"19 : {b1}"); // false
+            //b1 = numbers.Contains(30); // true
+            //Console.WriteLine($"30 : {b1}");
+
+            //numbers.Remove(15);
+            //numbers.RemoveAt(0);
+
+            //Console.WriteLine("*** Remove(15) RemoveAt(0) Collection ***");
+
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    Console.Write($"{numbers[i]} ");
+            //}
+            //Console.WriteLine();
+
+            //numbers.RemoveRange(0, 3);
+            //Console.WriteLine("*** RemoveRange(0, 3) Collection ***");
+
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    Console.Write($"{numbers[i]} ");
+            //}
+            //Console.WriteLine();
+
+            //numbers.Clear(); // removes all items
+            //Console.WriteLine($"number of items : {numbers.Count}");
+
+            #endregion Arraylist
+
+            #region Hastable
+
+            Hashtable students = new Hashtable();
+            students.Add(1, "Mahesh");
+            students.Add(2, "Suresh");
+            students.Add(3, "Ramesh");
+            // students.Add(3, "Kunal");
+
+            Console.WriteLine("*** ALL ITEMS ***");
+
+            foreach (DictionaryEntry item in students)
+            {
+                Console.WriteLine($"Key : {item.Key} Value : {item.Value}");
+            }
+
+            bool b1 = students.ContainsKey(4);
+            Console.WriteLine($"4 Key : {b1}");
+
+            b1 = students.ContainsValue("Mahesh");
+            Console.WriteLine($"Mahesh Value : {b1}");
+
+            var val = students[2];
+            Console.WriteLine($"2 : {val}");
+
+            #endregion Hastable
+
+            Console.ReadLine();
+        }
+    }
+}
