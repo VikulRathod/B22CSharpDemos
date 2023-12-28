74db5ebffd2e2aec337c9b05e20e45027ae072de
vikul rathod <vikul.rathod.net@gmail.com>
2023-12-28 15:06:51 +0530

    linq session 4

---
+++ 7_AggregationOperators/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_AggregationOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6 };
+
+            var c1 = numbers.Count();
+            Console.WriteLine($"Number of items : {c1}");
+
+            var c2 = numbers.LongCount();
+            Console.WriteLine($"Number of items : {c2}");
+
+            var c3 = numbers.Min();
+            Console.WriteLine($"Minimum value : {c3}"); // 1
+
+            var c4 = numbers.Max();
+            Console.WriteLine($"Maximum value : {c4}"); // 6
+
+            var c5 = numbers.Sum();
+            Console.WriteLine($"Sum : {c5}");
+
+            var c6 = numbers.Aggregate((i, j) => i + j);
+            Console.WriteLine($"Aggregate : {c6}");
+
+            var c7 = numbers.Average();
+            Console.WriteLine($"Average : {c7}");
+
+            bool b1 = numbers.Contains(5);
+            Console.WriteLine($"Contains(5) : {b1}"); // true
+            b1 = numbers.Contains(50);
+            Console.WriteLine($"Contains(50) : {b1}"); // false
+
+            // 1 2 3 4 5 6
+            // All() -> it checks whether all items matches given condition
+            b1 = numbers.All(i => i > 0);
+            Console.WriteLine($"All(i => i > 0) : {b1}"); // true
+
+            b1 = numbers.All(i => i > 1);
+            Console.WriteLine($"All(i => i > 1) : {b1}"); // false
+
+            // Any() -> it returns true even any one item matches given condition
+            b1 = numbers.Any(i => i > 5);
+            Console.WriteLine($"Any(i => i > 5) : {b1}"); // true
+
+            b1 = numbers.Any(i => i > 50);
+            Console.WriteLine($"Any(i => i > 50) : {b1}"); // false
+
+            // 1 2 3 4 5 6
+            b1 = numbers.All(i => i % 2 == 0);
+            Console.WriteLine($"All(i => i % 2 == 0) : {b1}"); // false
+
+            b1 = numbers.Any(i => i % 2 == 0);
+            Console.WriteLine($"Any(i => i % 2 == 0) : {b1}"); // true
+
+            Student s1 = new Student() { FirstName = "Vihaan", LastName = "Rathod" };
+            Student s2 = new Student() { FirstName = "Mihaan", LastName = "Rathod" };
+            Student s3 = new Student() { FirstName = "Krishika", LastName = "Rathod" };
+
+            List<Student> students = new List<Student>() { s1, s2, s3};
+
+            Student s4 = new Student() { FirstName = "Vihaan", LastName = "Rathod" };
+            bool b2 = students.Contains(s4);
+            Console.WriteLine(b2); // false
+
+            b2 = students.Contains(s4, new StudentHelper());
+            Console.WriteLine(b2); // true
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //public override bool Equals(object obj)
+        //{
+        //    Student s = obj as Student;
+        //    return this.FirstName.Equals(s.FirstName) &&
+        //        this.LastName.Equals(s.LastName);
+        //}
+
+        //public override int GetHashCode()
+        //{
+        //    return FirstName.GetHashCode() ^ LastName.GetHashCode();
+        //}
+    }
+
+    public class StudentHelper : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y)
+        {
+          return x.FirstName.Equals(y.FirstName) &&
+                x.LastName.Equals(y.LastName);
+        }
+
+        public int GetHashCode(Student obj)
+        {
+            return obj.FirstName.GetHashCode() ^ obj.LastName.GetHashCode();
+        }
+    }
+}
---
+++ 8_ElementsOperators/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_ElementsOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6 };
+
+            // ElementAt -> it returns item from given index
+            int item = numbers.ElementAt(0);
+            Console.WriteLine($"ElementAt(0) : {item}"); // 1
+            item = numbers.ElementAt(3);
+            Console.WriteLine($"ElementAt(3) : {item}"); // 4
+            //item = numbers.ElementAt(9);
+            //Console.WriteLine($"ElementAt(9) : {item}"); // 
+
+            // ElementAt -> throws exception when the given index is not within boundary
+            // ElementAtOrDefault -> works same like ELementAt()
+            // except it handles exception if given index is out of size and returns deafult value
+
+            item = numbers.ElementAtOrDefault(9);
+            Console.WriteLine($"ElementAtOrDefault(9) : {item}"); // 0
+
+            // First -> it returns first matching item from collection
+
+            item = numbers.First();
+            Console.WriteLine($"First() : {item}"); // 1
+
+            item = numbers.First(i => i % 2 == 0);
+            Console.WriteLine($"First(i => i % 2 == 0) : {item}"); // 2
+
+            //item = numbers.First(i => i > 50);
+            //Console.WriteLine($"First(i => i > 50) : {item}");
+
+            // FIrst -> throws exception if no item matching condition
+            // FirstOrDefault -> it handles exception even if there are no matching item and returns default value
+
+            item = numbers.FirstOrDefault(i => i > 50);
+            Console.WriteLine($"FirstOrDefault(i => i > 50) : {item}"); // 0
+
+            // Last -> returns last matching item from collection
+            item = numbers.Last();
+            Console.WriteLine($"Last() : {item}"); // 6
+
+            item = numbers.Last(i => i % 2 == 0);
+            Console.WriteLine($"Last(i => i % 2 == 0) : {item}"); // 6
+
+            //item = numbers.Last(i => i > 50); // runtime error
+            //Console.WriteLine($"Last(i => i > 50) : {item}");
+
+            // Last -> throws exception when there are no matching item
+            // LastOrDefault -> handles this exception
+
+            item = numbers.LastOrDefault(i => i > 50);
+            Console.WriteLine($"LastOrDefault(i => i > 50) : {item}"); // 0
+
+            //// Single -> returns first matching item from collection
+            //// Single -> throws exception if there are multiple matching items
+            //// numbers = new List<int>() { 3 };
+            //item = numbers.Single();
+            //Console.WriteLine($"Single() : {item}");
+
+            // item = numbers.Single(i => i % 2 == 0);
+            item = numbers.Single(i => i > 5);
+            Console.WriteLine($"Single(i => i > 5) : {item}");
+
+            //item = numbers.Single(i => i > 50);
+            //Console.WriteLine($"Single(i => i > 50) : {item}");
+
+            // Single -> throws exception when no matching item found
+            // SingleOrDefault -> it handles exception if no matching item found and returns default value
+
+            item = numbers.SingleOrDefault(i => i > 50);
+            Console.WriteLine($"SingleOrDefault(i => i > 50) : {item}");
+
+            //item = numbers.SingleOrDefault(i => i % 2 == 0);
+            //Console.WriteLine($"SingleOrDefault(i => i % 2 == 0) : {item}");
+
+            Console.ReadLine();
+        }
+    }
+}
