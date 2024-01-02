317f9f765dd0f5ca3c15e833726b5d3def0b1422
vikul rathod <vikul.rathod.net@gmail.com>
2024-01-02 12:44:45 +0530

    linq session 5

---
+++ 10_PartitioningOperators/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_PartitioningOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Take
+
+            //List<int> numbers = new List<int>() { 2, 4, 7, 6, 5, 3, 10, 15, 20, 9 };
+
+            //Console.WriteLine("*** ALL NUMBERS ***");
+            //foreach (var item in numbers)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine();
+
+            //IEnumerable<int> result = numbers.Take(5);
+            //Console.WriteLine("*** Take(3) ***");
+            //foreach (var item in result)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine();
+
+            //// IEnumerable<int> result1 = numbers.TakeWhile(i => i < 10);
+            //// IEnumerable<int> result1 = numbers.TakeWhile(i => i % 2 == 0);
+            //IEnumerable<int> result1 = numbers.TakeWhile(i => i > 10);
+            //Console.WriteLine("*** TakeWhile(i => i < 10) ***");
+            //foreach (var item in result1)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine();
+
+            #endregion Take
+
+            #region Skip
+
+            //List<int> numbers = new List<int>() { 2, 4, 7, 6, 5, 3, 10, 15, 20, 9 };
+
+            //Console.WriteLine("*** ALL NUMBERS ***");
+            //foreach (var item in numbers)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine();
+
+            //IEnumerable<int> result = numbers.Skip(5);
+
+            //Console.WriteLine("*** Skip(5) ***");
+            //foreach (var item in result)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine();
+
+            //IEnumerable<int> result1 = numbers.SkipWhile(i => i < 10);
+            //Console.WriteLine("*** SkipWhile(i => i < 10) ***");
+            //foreach (var item in result1)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine();
+
+            #endregion Skip
+
+            #region Second highest number
+
+            List<int> numbers = new List<int>() { 10, 17, 23, 54, 47 };
+
+            int max = numbers.Max();
+            Console.WriteLine($"Highest Number : {max}");
+
+            //int secondHighest =
+            //    numbers.OrderByDescending(i => i)
+            //    .Take(2).OrderBy(i => i)
+            //    .Take(1).FirstOrDefault();
+
+            //int secondHighest =
+            //    numbers.OrderByDescending(i => i)
+            //    .Take(2).Skip(1).FirstOrDefault();
+
+            int secondHighest =
+                numbers.OrderByDescending(i => i)
+                .Take(2).Min();
+            Console.WriteLine($"Second Highest Number : {secondHighest}");
+
+            #endregion Second highest number
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 9_SetOperators/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_SetOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Distinct
+
+            //List<int> numbers = new List<int>() { 1, 2, 4, 2, 5, 4, 2 };
+
+            //var unique = numbers.Distinct();
+            //foreach (var item in unique)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine();
+
+            #endregion Distinct
+
+            #region Union
+
+            //List<int> numbers1 = new List<int>() { 3, 2, 1 };
+            //List<int> numbers2 = new List<int>() { 5, 6, 4, 2 };
+
+            //// IEnumerable<int> result = numbers1.Union(numbers2); // ignoring duplicates
+
+            //IEnumerable<int> result = numbers1.Concat(numbers2);
+
+            //foreach (var item in result)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine();
+
+            #endregion Union
+
+            #region Intersect
+
+            //List<int> numbers1 = new List<int>() { 3, 2, 5, 1 };
+            //List<int> numbers2 = new List<int>() { 4, 2, 1 };
+
+            //// IEnumerable<int> result = numbers1.Intersect(numbers2);
+
+            //IEnumerable<int> result = numbers1.Except(numbers2);
+
+            //foreach (var item in result)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine();
+
+            #endregion Intersect
+
+            #region SequenceEqual
+
+            List<int> numbers1 = new List<int>() { 2, 3, 1 };
+            List<int> numbers2 = new List<int>() { 1, 3, 2 };
+
+            //numbers1.Sort();
+            //numbers2.Sort();
+
+            bool b1 = numbers1.Count == numbers2.Count;
+            Console.WriteLine("NOT EQUAL");
+
+            b1 = numbers1.SequenceEqual(numbers2);
+            Console.WriteLine($"SequenceEqual : {b1}");
+
+            #endregion SequenceEqual
+
+            Console.ReadLine();
+        }
+    }
+}
